9ed72b9600f68266872b7fdffcd59154cfb335d6
David Honegger Rogers <dhr@lanl.gov>
2020-06-04 10:25:35 -0600

    simplified example

--- src/tests/dbcreate.cxx
+++ src/tests/dbcreate.cxx
@@ -1,24 +1,19 @@
-#include <string>
-#include <iostream>
-#include <unistd.h>
-#include <stdio.h>
+// #include <string>
+// #include <iostream>
+// #include <unistd.h>
+// #include <stdio.h>
 #include "CinBase.h"
 #include "CinDBWriter.h"
-#include "vtkXMLUnstructuredGridReader.h"
-#include "vtkSmartPointer.h"
 
 int main() 
 {
-    // set up path
-    char cwd[1024];
-    getcwd(cwd, sizeof(cwd));
-    std::string db_path = cwd;
-    db_path += "/zap.cdb";
-
     // create a writer
-    CinDBWriter writer(db_path);
+    CinDBWriter writer("example.cdb");
 
-    // writer.setInput( reader->GetOutput() );
+    // set the input file 
+    writer.setInputFile("some.vtu");
+
+    // set camera positions 
     writer.addCameraPosition(  0.0, 45.0);
     writer.addCameraPosition( 10.0, 45.0);
     writer.addCameraPosition( 20.0, 45.0);
@@ -30,8 +25,7 @@ int main()
     writer.addCameraPosition( 80.0, 45.0);
     writer.addCameraPosition( 90.0, 45.0);
 
-    // writer.write();
-    writer.setInputFile("/Users/dhr/LANL/data/hacc/halo_hacc/results/extracted/extracted_8.vtu");
+    // write the database
     writer.write();
 
     return 1;
